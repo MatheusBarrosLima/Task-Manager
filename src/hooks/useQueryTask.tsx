307d29a112452bed41c4ccb25126bf47898a53a4
Matheus Barros Lima <matheusmatheusbarroslima6@gmail.com>
2024-06-10 15:17:37 -0300

    add navigation tasks

--- src/hooks/useQueryTask.tsx
+++ src/hooks/useQueryTask.tsx
@@ -1,7 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import { API } from "../configs/api";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { TaskDataTypes } from "../components/TaskCard";
+import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
 
 type FilterType = "all" | "completed" | "pending" | "late";
 
@@ -10,6 +11,10 @@ export function useQueryTask() {
   const [totalPages, setTotalPages] = useState(0);
   const [limit, setLimit] = useState(10);
   const [filter, setFilter] = useState<FilterType>("all");
+
+  const location = useLocation();
+  const navigate = useNavigate();
+  const searchParams = useSearchParams()
 
   async function getTasks({ page = 1, limit = 10, filter = "all" }) {
     if (page <= 0) page = 1;
@@ -58,12 +63,14 @@ export function useQueryTask() {
   function nextPage() {
     if (page < totalPages) {
       setPage((prevPage) => prevPage + 1);
+      navigate(`?filter=${filter}&page=${page + 1}`)
     }
   }
 
   function prevPage() {
     if (page > 1) {
       setPage((prevPage) => prevPage - 1);
+      navigate(`?filter=${filter}&page=${page - 1}`)
     }
   }
 
@@ -78,6 +85,29 @@ export function useQueryTask() {
   function changeFilter(value: FilterType) {
     setFilter(value);
   }
+
+useEffect(() => {
+  if(location.pathname != "/tasks") return;
+
+  const pageQuery = Number(searchParams[0].get("page"));
+  const filterQuery = searchParams[0].get("filter") as FilterType;
+
+  setPage(pageQuery || 1)
+  setFilter(filterQuery || "all")
+
+  if(totalPages > 0){
+    if(pageQuery> totalPages){
+      navigate(`?filter=${filterQuery}&page=${totalPages}`);
+      setPage(totalPages);
+      return
+    }
+    if(pageQuery< 1){
+      navigate(`?filter=${filterQuery}&page=1`)
+      setPage(1)
+      return
+    }
+  }
+},[page, totalPages, searchParams,navigate,location])
 
   const query = useQuery({
     queryKey: ["tasksData", page, limit, filter],
